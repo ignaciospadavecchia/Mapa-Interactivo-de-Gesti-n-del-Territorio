2cb28834bdb4701486267284c796f58de3057d1f
Ignacio Spadavecchia <isvekia@tuta.io>
2025-02-25 23:26:08 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  mapboxToken: 'pk.eyJ1IjoiaW5hdHhpbyIsImEiOiJjbTNqeHJpMGowOHU5MmtxeXBvcGF6MXhxIn0.5MYT1IFmmy_f9Ar1E_9xGg',
+  mapboxToken: '',
   apiUrl: 'http://localhost:3000/api'
 }; 
